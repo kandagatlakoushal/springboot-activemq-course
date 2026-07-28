933defff21ac00423c34d4a19958789579fc1071
k-Koushal <kandagatlakoushal@gmail.com>
2026-07-28 13:01:01 +0530

    feat: initialize Spring Boot project structure for Notification Hub service

---
+++ Projects/Project 1 - Notification Hub/NotificationHub 2/src/test/java/com/learningPath/springProfileAmq/NotificationHub/NotificationHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningPath.springProfileAmq.NotificationHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
